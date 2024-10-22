8617c98d908e46c554ce9f2a372a54e8e87f712e
WAD3n <gluchowski.hubert@gmail.com>
2024-10-22 13:59:18 +0200

    loginPage

--- Website/reactapp/src/App.js
+++ Website/reactapp/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          Hello world
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Login</h1>
+        <div className="login-container">
+          <input
+            type="text"
+            className="login-input"
+            placeholder="Username"
+          />
+          <input
+            type="password"
+            className="login-input"
+            placeholder="Password"
+          />
+          <button className="login-button">Submit</button>
+        </div>
       </header>
     </div>
   );
